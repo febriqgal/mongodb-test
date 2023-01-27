ca87e6f4a1fa0da1fcf3eff661aeb5d932cbe95a
Febriqgal Purnama <febriqgalp@gmail.com>
2023-01-28 06:50:34 +0700

    gggggggggggggg

--- src/db/mongodb.js
+++ src/db/mongodb.js
@@ -1,5 +1,6 @@
 import { MongoClient } from "mongodb";
-const uri = process.env.MONGODB_URI;
+const uri =
+  "mongodb+srv://aye:XNVLfVE2yp4IDlvC@cluster0.q1ksemt.mongodb.net/test";
 
 const client = new MongoClient(uri, {
   useUnifiedTopology: true,
